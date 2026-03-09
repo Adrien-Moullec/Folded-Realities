48998daa4920bf228cb4bdf6f89d1a78ad17233e
AdrienMoullec <amoullec1@gmail.com>
2026-03-09 20:59:19 +0000

    Animation smoothing

--- Assets/Scripts/Abilities/AbilityAnimation.cs
+++ Assets/Scripts/Abilities/AbilityAnimation.cs
@@ -7,12 +7,17 @@ namespace AbilitySystem
     public class AbilityAnimation
     {
         public AnimationClip animation;
-        public AnimationPlayStyle animationPlayStyle;
+        public WrapMode animationPlayStyle;
         public float speed = 1;
         public float weight = 1;
         public float crossFadeTime = 0.2f;
         [Range(0f, 1f)] public float abilityEventDelta = 1;
         public AnimationCurve weightOverTime;
+        public string clipName
+        {
+            get => animation.name;
+            set => animation.name = value;
+        }
     }
     public enum AnimationPlayStyle
     {
