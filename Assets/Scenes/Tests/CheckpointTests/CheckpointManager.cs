ecc774b4ecac8f2935964a2bef84861a9cf16e92
AHannis <s4108628@glos.ac.uk>
2026-03-10 16:08:05 +0000

    mixamo bot checkpoint stuff
    checkpoint stuff

--- Assets/Scenes/Tests/CheckpointTests/CheckpointManager.cs
+++ Assets/Scenes/Tests/CheckpointTests/CheckpointManager.cs
@@ -1,26 +1,29 @@
 using UnityEngine;
 
-public class CheckpointManager : MonoBehaviour
-{
+public class CheckpointManager : MonoBehaviour {
     public static CheckpointManager Instance;
 
     private Vector3 lastCheckpointPosition;
     private bool hasCheckpoint = false;
 
-    private void Awake()
-    {
+    private void Awake() {
         Instance = this;
     }
 
-    public void SetCheckpoint(Vector3 position)
-    {
+    public void SetCheckpoint(Vector3 position) {
         lastCheckpointPosition = position;
         hasCheckpoint = true;
+
+        Debug.Log("Checkpoint reached at: " + position);
     }
 
-    public void RespawnPlayer(GameObject playerRoot)
-    {
-        if (!hasCheckpoint) return;
+    public void RespawnPlayer(GameObject playerRoot) {
+        if (!hasCheckpoint) {
+            Debug.Log("No checkpoint set!");
+            return;
+        }
+
+        Debug.Log("Respawning player at checkpoint");
 
         playerRoot.transform.position = lastCheckpointPosition;
     }
