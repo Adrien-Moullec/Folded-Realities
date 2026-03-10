ad8282a863c1db0c9471f162290b2e062b00087f
AHannis <s4108628@glos.ac.uk>
2026-03-10 16:41:43 +0000

    checkpoints
    yay checkpoints work - at the moment press k to simulate death, and respawns player at nearest checkpoint lights up green when activated and registers player getting to last checkpoint even if they fall and end up back at the first

--- Assets/Scenes/Tests/CheckpointTests/Checkpoint.cs
+++ Assets/Scenes/Tests/CheckpointTests/Checkpoint.cs
@@ -1,12 +1,59 @@
 using UnityEngine;
 
-public class Checkpoint : MonoBehaviour
-{
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            CheckpointManager.Instance.SetCheckpoint(transform.position);
+public class Checkpoint : MonoBehaviour {
+    public int checkpointIndex;
+
+    public Renderer checkpointRenderer;
+
+    public Color inactiveColor = Color.red;
+    public Color activeColor = Color.green;
+
+    private bool activated = false;
+
+    private Material checkpointMaterial;
+
+    void Start() {
+        if (checkpointRenderer != null) {
+            // Create unique material instance
+            checkpointMaterial = checkpointRenderer.material;
+
+            SetColor(inactiveColor);
         }
+    }
+
+    void SetColor(Color c) {
+        if (checkpointMaterial == null) {
+            return;
+        }
+
+        if (checkpointMaterial.HasProperty("_BaseColor")) {
+            checkpointMaterial.SetColor("_BaseColor", c);
+        }
+
+        if (checkpointMaterial.HasProperty("_Color")) {
+            checkpointMaterial.SetColor("_Color", c);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (!other.CompareTag("Player")) {
+            return;
+        }
+
+        if (activated) {
+            return;
+        }
+
+        CheckpointManager.Instance.SetCheckpoint(transform.position + Vector3.up * 1f, checkpointIndex);
+
+        ActivateCheckpoint();
+    }
+
+    void ActivateCheckpoint() {
+        activated = true;
+
+        SetColor(activeColor);
+
+        Debug.Log("Checkpoint " + checkpointIndex + " activated");
     }
 }
--- Assets/Scenes/Tests/CheckpointTests/CheckpointManager.cs
+++ Assets/Scenes/Tests/CheckpointTests/CheckpointManager.cs
@@ -4,27 +4,40 @@ public class CheckpointManager : MonoBehaviour {
     public static CheckpointManager Instance;
 
     private Vector3 lastCheckpointPosition;
-    private bool hasCheckpoint = false;
+    private int currentCheckpointIndex = -1;
 
     private void Awake() {
         Instance = this;
     }
 
-    public void SetCheckpoint(Vector3 position) {
-        lastCheckpointPosition = position;
-        hasCheckpoint = true;
+    public void SetCheckpoint(Vector3 position, int checkpointIndex) {
+        // Prevent activating old checkpoints
+        if (checkpointIndex <= currentCheckpointIndex) {
+            return;
+        }
 
-        Debug.Log("Checkpoint reached at: " + position);
+        currentCheckpointIndex = checkpointIndex;
+        lastCheckpointPosition = position;
+
+        Debug.Log("Checkpoint reached: " + checkpointIndex);
     }
 
     public void RespawnPlayer(GameObject playerRoot) {
-        if (!hasCheckpoint) {
+        if (currentCheckpointIndex == -1) {
             Debug.Log("No checkpoint set!");
             return;
         }
 
         Debug.Log("Respawning player at checkpoint");
 
-        playerRoot.transform.position = lastCheckpointPosition;
+        CharacterController cc = playerRoot.GetComponent<CharacterController>();
+
+        if (cc != null) {
+            cc.enabled = false;
+            playerRoot.transform.position = lastCheckpointPosition;
+            cc.enabled = true;
+        } else {
+            playerRoot.transform.position = lastCheckpointPosition;
+        }
     }
 }
