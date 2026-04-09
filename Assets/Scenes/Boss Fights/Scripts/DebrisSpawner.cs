81e12573d28ff5c7e06726c9977f8e99a4bd2ac0
AHannis <s4108628@glos.ac.uk>
2026-04-09 14:55:48 +0100

    npc can be caught :)

--- Assets/Scenes/Boss Fights/Scripts/DebrisSpawner.cs
+++ Assets/Scenes/Boss Fights/Scripts/DebrisSpawner.cs
@@ -1,43 +1,33 @@
 using UnityEngine;
-using System.Collections.Generic;
 
 public class DebrisSpawner : MonoBehaviour {
     [Header("Prefabs")]
     public GameObject[] debrisPrefabs;
 
     [Header("References")]
-    public Transform player;
     public Transform npc;
 
     [Header("Spawn Settings")]
     public int spawnCount = 50;
     public Vector3 areaSize = new Vector3(20, 50, 20);
 
-    [Header("Safety")]
-    public float npcSafeRadius = 2f;
-    public float playerSafeRadius = 2.5f;
-
-    [Header("Spacing")]
-    public float minDistanceBetweenDebris = 3f;
-
-    private List<Vector3> spawnedPositions = new List<Vector3>();
+    [Header("NPC Safe Zone")]
+    public float npcSafeRadius = 3f;
 
     void Start() {
-        
         for (int i = 0; i < spawnCount; i++) {
             SpawnDebris(false);
         }
 
-       
         for (int i = 0; i < spawnCount; i++) {
-            SpawnDebris(true);
+            SpawnDebris(true); 
         }
     }
 
     void SpawnDebris(bool bottomLayer) {
         Vector3 randomPos = transform.position;
 
-        for (int i = 0; i < 15; i++) {
+        for (int i = 0; i < 10; i++) {
             float yMin = bottomLayer ? -areaSize.y / 2f : -areaSize.y / 4f;
             float yMax = bottomLayer ? -areaSize.y / 4f : areaSize.y / 2f;
 
@@ -50,21 +40,8 @@ public class DebrisSpawner : MonoBehaviour {
             bool tooCloseToNPC = npc != null &&
                 Vector3.Distance(testPos, npc.position) < npcSafeRadius;
 
-            
-            bool tooCloseToPlayer = player != null &&
-                Mathf.Abs(testPos.x - player.position.x) < playerSafeRadius;
-
-            bool tooCloseToOther = false;
-            foreach (Vector3 pos in spawnedPositions) {
-                if (Vector3.Distance(testPos, pos) < minDistanceBetweenDebris) {
-                    tooCloseToOther = true;
-                    break;
-                }
-            }
-
-            if (!tooCloseToNPC && !tooCloseToPlayer && !tooCloseToOther) {
+            if (!tooCloseToNPC) {
                 randomPos = testPos;
-                spawnedPositions.Add(randomPos);
                 break;
             }
         }
@@ -73,7 +50,6 @@ public class DebrisSpawner : MonoBehaviour {
 
         GameObject obj = Instantiate(prefab, randomPos, Random.rotation);
 
-        // Big readable spheres
         float scale = Random.Range(2f, 3.5f);
         obj.transform.localScale = Vector3.one * scale;
     }
