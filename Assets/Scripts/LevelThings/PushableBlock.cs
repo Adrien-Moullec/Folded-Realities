6a32ab5e7335c634689ad772525aa7f39796bce6
AHannis <s4108628@glos.ac.uk>
2026-04-13 19:39:43 +0100

    more camera

--- Assets/Scripts/LevelThings/PushableBlock.cs
+++ Assets/Scripts/LevelThings/PushableBlock.cs
@@ -1,29 +1,22 @@
 using UnityEngine;
 
-public class PushableBlock : MonoBehaviour
-{
+public class PushableBlock : MonoBehaviour {
     private Rigidbody rb;
     private bool isBeingPushed = false;
 
-    void Start()
-    {
+    void Start() {
         rb = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate()
-    {
-        if (!isBeingPushed)
-        {
-            
+    void FixedUpdate() {
+        if (!isBeingPushed) {
             rb.linearVelocity = new Vector3(0, rb.linearVelocity.y, 0);
         }
 
-        
         isBeingPushed = false;
     }
 
-    public void SetPushed()
-    {
+    public void SetPushed() {
         isBeingPushed = true;
     }
 }
