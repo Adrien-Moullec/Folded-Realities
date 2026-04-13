bd6f818a4077df7d5c8f3d2b3d259a298211e925
AHannis <s4108628@glos.ac.uk>
2026-04-13 12:59:13 +0100

    boss level changes

--- Assets/Scenes/Boss Fights/Scripts/Phase1/Player/SlowFallOverride.cs
+++ Assets/Scenes/Boss Fights/Scripts/Phase1/Player/SlowFallOverride.cs
@@ -2,26 +2,60 @@ using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
 public class SlowFallOverride : MonoBehaviour {
-    public float fallSpeed = 2f;
+    public float fallSpeed = 0.5f;
+
+    [SerializeField] private MonoBehaviour playerController;
+    [SerializeField] private Transform npc;
+    public float minHeightAboveNPC = 2f;
 
     private CharacterController controller;
-    private bool canFall = true;
+    private bool isFalling = true;
 
     void Start() {
         controller = GetComponent<CharacterController>();
+
+        if (playerController != null) {
+            playerController.enabled = false;
+        }
+
+        controller.enabled = false;
     }
 
     void Update() {
-        if (!canFall) {
+        if (!isFalling) {
             return;
         }
 
-        if (!controller.isGrounded) {
-            controller.Move(Vector3.down * fallSpeed * Time.deltaTime);
+        Vector3 pos = transform.position;
+        pos.y -= fallSpeed * Time.deltaTime;
+
+        if (npc != null) {
+            if (pos.y <= npc.position.y + minHeightAboveNPC) {
+                pos.y = npc.position.y + minHeightAboveNPC;
+            }
+        }
+
+        transform.position = pos;
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.5f)) {
+            Land();
         }
     }
 
+    void Land() {
+        isFalling = false;
+
+        controller.enabled = true;
+
+        if (playerController != null) {
+            playerController.enabled = true;
+        }
+
+        this.enabled = false;
+    }
+
     public void StopFalling() {
-        canFall = false;
+        Land();
     }
 }
