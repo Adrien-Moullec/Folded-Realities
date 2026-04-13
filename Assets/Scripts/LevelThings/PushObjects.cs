c491c798baf78bd6707edbcc8c074983ce63e6df
AHannis <s4108628@glos.ac.uk>
2026-04-13 19:39:43 +0100

    more camera

--- Assets/Scripts/LevelThings/PushObjects.cs
+++ Assets/Scripts/LevelThings/PushObjects.cs
@@ -1,26 +1,21 @@
 using UnityEngine;
 
 public class PushObjects : MonoBehaviour {
-
-    public float pushForce = 5f;
+    public float pushSpeed = 3f;
 
     void OnControllerColliderHit(ControllerColliderHit hit) {
-
         if (!hit.collider.CompareTag("Pushable")) {
-            return;
-        }
-
-        Rigidbody rb = hit.collider.attachedRigidbody;
-        if (rb == null) {
             return;
         }
 
         Vector3 moveDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
 
-        if (moveDir.magnitude < 0.1f) {
+        if (moveDir.magnitude < 0.5f) {
             return;
         }
 
-        rb.AddForce(moveDir * pushForce, ForceMode.Impulse);
+        Transform block = hit.collider.transform;
+
+        block.position += moveDir.normalized * pushSpeed * Time.deltaTime;
     }
 }
