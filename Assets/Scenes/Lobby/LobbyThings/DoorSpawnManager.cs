514c1c8dd7358cc3801b29afd51b33dadc1c2052
AHannis <s4108628@glos.ac.uk>
2026-04-16 13:41:43 +0100

    lobby things

--- Assets/Scenes/Lobby/LobbyThings/DoorSpawnManager.cs
+++ Assets/Scenes/Lobby/LobbyThings/DoorSpawnManager.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 
 public class DoorSpawnManager : MonoBehaviour {
-    public Transform[] spawnPoints;
+    [SerializeField] Transform[] doorSpawnPoints;
+    [SerializeField] Transform defaultSpawnPoint;
 
     void Start() {
         StartCoroutine(SetSpawn());
@@ -10,20 +11,35 @@ public class DoorSpawnManager : MonoBehaviour {
     System.Collections.IEnumerator SetSpawn() {
         yield return null; // wait 1 frame
 
-        int spawnID = PlayerPrefs.GetInt("SpawnPoint", 0);
-
-        if (spawnID >= spawnPoints.Length) {
-            spawnID = 0;
-        }
-
         GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player == null) yield break;
 
-        if (player != null) {
-            CharacterController cc = player.GetComponent<CharacterController>();
+        CharacterController cc = player.GetComponent<CharacterController>();
+
+        if (PlayerPrefs.GetInt("UseDoorSpawn", 0) == 1) {
+            int doorID = PlayerPrefs.GetInt("SpawnDoorID", 0);
+
+            if (doorID >= 0 && doorID < doorSpawnPoints.Length) {
+                Transform spawn = doorSpawnPoints[doorID];
+
+                if (cc != null) cc.enabled = false;
+
+                // Slight upward offset ONLY (not forward)
+                Vector3 safePos = spawn.position + Vector3.up * 0.5f;
+
+                player.transform.position = safePos;
+                player.transform.rotation = spawn.rotation;
+
+                if (cc != null) cc.enabled = true;
+            }
+
+            PlayerPrefs.SetInt("UseDoorSpawn", 0);
+        } else if (defaultSpawnPoint != null) {
 
             if (cc != null) cc.enabled = false;
 
-            player.transform.position = spawnPoints[spawnID].position;
+            player.transform.position = defaultSpawnPoint.position;
+            player.transform.rotation = defaultSpawnPoint.rotation;
 
             if (cc != null) cc.enabled = true;
         }
