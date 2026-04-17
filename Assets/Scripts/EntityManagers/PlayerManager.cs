a461657ed8a66f9ee997b02fb45b3b700b3f4932
jayjay foster <jasperfosterwork@gmail.com>
2026-04-17 20:17:51 +0100

    Pipeline shenanigans and unity stuff

--- Assets/Scripts/EntityManagers/PlayerManager.cs
+++ Assets/Scripts/EntityManagers/PlayerManager.cs
@@ -9,7 +9,8 @@ using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(PlayerInput))]
 [RequireComponent(typeof(PlayerAbilityController))]
-public class PlayerManager : MonoBehaviour {
+public class PlayerManager : MonoBehaviour
+{
     public static PlayerManager player;
     #region Variables
     [Space]
@@ -32,8 +33,11 @@ public class PlayerManager : MonoBehaviour {
     [SerializeField] IAbility iAbility;
     [SerializeField] RadialMenuManager _RadialMenuManager;
     private PlayerInput _PlayerInput;
-    [SerializeField] private AreaColliderCheck interactArea;
-    private PlayerAbilityController _playerAbilityController;
+
+    [Space]
+    [Header("Interaction")]
+    [SerializeField] GameObject body;
+    [SerializeField] AreaColliderCheck interactionArea;
 
     [Space]
     [Header("Inputs")]
@@ -46,15 +50,18 @@ public class PlayerManager : MonoBehaviour {
     InputAction ability2Input;
     InputAction ability3Input;
     InputAction radialWheel;
+    InputAction interact;
     bool wheelActive = false;
     #endregion
 
-    void Awake() {
+    void Awake()
+    {
         player = this;
         iAbility = GetComponent<IAbility>();
     }
     #region On Start
-    private void OnEnable() {
+    private void OnEnable()
+    {
         iAbility = GetComponent<PlayerAbilityController>();
         _PlayerInput = GetComponent<PlayerInput>();
         Cursor.lockState = CursorLockMode.Locked;
@@ -67,16 +74,19 @@ public class PlayerManager : MonoBehaviour {
         ability1Input = _PlayerInput.actions["Ability1"];
         ability2Input = _PlayerInput.actions["Ability2"];
         ability3Input = _PlayerInput.actions["Ability3"];
+        interact = _PlayerInput.actions["Interact"];
 
         lookInput.performed += input => deltaLook = input.ReadValue<Vector2>();
         lookInput.canceled += input => deltaLook = input.ReadValue<Vector2>();
         runInput.performed += input => iAbility.GetInputValues.SetRunToggle(true);
         runInput.canceled += input => iAbility.GetInputValues.SetRunToggle(false);
-        radialWheel.performed += input => {
+        radialWheel.performed += input =>
+        {
             _RadialMenuManager?.SetWheelActive(true);
             wheelActive = true;
         };
-        radialWheel.canceled += input => {
+        radialWheel.canceled += input =>
+        {
             _RadialMenuManager?.SetWheelActive(false);
             wheelActive = false;
             iAbility.InputTransitionName(_RadialMenuManager?.OnSegmentClicked());
@@ -88,16 +98,19 @@ public class PlayerManager : MonoBehaviour {
         ability3Input.performed += input => iAbility.GetInputValues.isTertiaryAbility = true;
         ability3Input.canceled += input => iAbility.GetInputValues.isTertiaryAbility = false;
     }
-    void OnDisable() {
+    void OnDisable()
+    {
         lookInput.performed -= input => deltaLook = input.ReadValue<Vector2>();
         lookInput.canceled -= input => deltaLook = input.ReadValue<Vector2>();
         runInput.performed -= input => iAbility.GetInputValues.SetRunToggle(true);
         runInput.canceled -= input => iAbility.GetInputValues.SetRunToggle(false);
-        radialWheel.performed -= input => {
+        radialWheel.performed -= input =>
+        {
             _RadialMenuManager?.SetWheelActive(true);
             wheelActive = true;
         };
-        radialWheel.canceled -= input => {
+        radialWheel.canceled -= input =>
+        {
             _RadialMenuManager?.SetWheelActive(false);
             wheelActive = false;
             iAbility.InputTransitionName(_RadialMenuManager?.OnSegmentClicked());
@@ -111,13 +124,24 @@ public class PlayerManager : MonoBehaviour {
     }
     #endregion
 
+    void OnInteract()
+    {
+        RaycastHit[] hits = new RaycastHit[4];
+        interactionArea.GetColliders(body).Invoke(hits);
+        foreach (var n in hits)
+            if (n.collider.TryGetComponent(out IInteractable iinteract))
+                iinteract.OnInteract();
+    }
+
     #region Update Functions
-    private void Update() {
+    private void Update()
+    {
         Movement();
     }
 
     #region Camera
-    void Movement() {
+    void Movement()
+    {
 
         if (wheelActive) return;
         Vector2 m = moveInput.ReadValue<Vector2>();
@@ -129,10 +153,10 @@ public class PlayerManager : MonoBehaviour {
         iAbility.GetInputValues.SetDirection(camDir);
     }
     #endregion
-
-    private void OnDrawGizmos() {
-        if (_playerAbilityController == null) _playerAbilityController = GetComponent<PlayerAbilityController>();
-        interactArea.Gizmo(_playerAbilityController.BodyHolder);
-    }
     #endregion
+
+    private void OnDrawGizmos()
+    {
+        interactionArea.Gizmo(body);
+    }
 }
--- Assets/Scripts/Player/AreaColliderCheck.cs
+++ Assets/Scripts/Player/AreaColliderCheck.cs
@@ -5,45 +5,56 @@ using UnityEngine;
 using UnityEditor;
 
 [Serializable]
-public class AreaColliderCheck {
+public class AreaColliderCheck
+{
     public CheckShape checkShape;
     public Vector3 centerOffset = Vector3.zero;
     public float size1 = 1;
     public Vector3 halfExtents = Vector3.one;
     public LayerMask layers = 1;
     public bool doDrawGizmo = true;
+    public bool wireFrame = true;
 
     public Action<RaycastHit[]> GetColliders(GameObject gameObject) => GetColliders(gameObject.transform.position, gameObject.transform.forward);
 
-    public Action<RaycastHit[]> GetColliders(Vector3 location, Vector3 direction) {
+    public Action<RaycastHit[]> GetColliders(Vector3 location, Vector3 direction)
+    {
         Quaternion rotation = Quaternion.LookRotation(direction);
         Vector3 pos = location + rotation * centerOffset;
-        return checkShape switch {
-            CheckShape.Sphere => (RaycastHit[] x) => {
+        return checkShape switch
+        {
+            CheckShape.Sphere => (RaycastHit[] x) =>
+            {
                 Physics.SphereCastNonAlloc(pos, size1, direction, x, size1, layers);
             }
             ,
-            CheckShape.Cube => (RaycastHit[] x) => {
+            CheckShape.Cube => (RaycastHit[] x) =>
+            {
                 Physics.BoxCastNonAlloc(pos, halfExtents, direction, x, rotation, Mathf.Max(halfExtents.y, halfExtents.z), layers);
             }
             ,
             _ => null,
         };
     }
-    public void Gizmo(GameObject gameObject) => Gizmo(gameObject.transform.position, gameObject.transform.forward);
-    public void Gizmo(Vector3 location, Vector3 direction) {
+    public void Gizmo(GameObject gameObject) => Gizmo(gameObject.transform);
+    public void Gizmo(Transform trans) => Gizmo(trans.position, trans.forward);
+    public void Gizmo(Vector3 location, Vector3 direction)
+    {
         if (!doDrawGizmo) return;
         Quaternion rotation = Quaternion.LookRotation(direction);
 
         Gizmos.matrix = Matrix4x4.TRS(location + rotation * centerOffset, rotation, Vector3.one);
         Gizmos.color = Color.red;
-        switch (checkShape) {
+        switch (checkShape)
+        {
             case CheckShape.Sphere:
-                Gizmos.DrawWireSphere(Vector3.zero, size1);
+                if (wireFrame) Gizmos.DrawWireSphere(Vector3.zero, size1);
+                else Gizmos.DrawSphere(Vector3.zero, size1);
                 break;
 
             case CheckShape.Cube:
-                Gizmos.DrawWireCube(Vector3.zero, halfExtents);
+                if (wireFrame) Gizmos.DrawWireCube(Vector3.zero, halfExtents);
+                else Gizmos.DrawCube(Vector3.zero, halfExtents);
                 break;
         }
         Gizmos.matrix = Matrix4x4.identity;
@@ -51,8 +62,10 @@ public class AreaColliderCheck {
 }
 
 [CustomPropertyDrawer(typeof(AreaColliderCheck))]
-public class AreaAffectsDrawer : PropertyDrawer {
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+public class AreaAffectsDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
         EditorGUI.BeginProperty(position, label, property);
 
         property.isExpanded = EditorGUI.Foldout(
@@ -61,7 +74,8 @@ public class AreaAffectsDrawer : PropertyDrawer {
             label
         );
 
-        if (property.isExpanded) {
+        if (property.isExpanded)
+        {
             EditorGUI.indentLevel++;
 
             float y = position.y + EditorGUIUtility.singleLineHeight;
@@ -79,13 +93,15 @@ public class AreaAffectsDrawer : PropertyDrawer {
 
             DrawProp(ref y, position, property, "layers");
             DrawProp(ref y, position, property, "doDrawGizmo");
+            DrawProp(ref y, position, property, "wireFrame");
 
             EditorGUI.indentLevel--;
         }
         EditorGUI.EndProperty();
     }
 
-    void DrawProp(ref float y, Rect position, SerializedProperty property, string name) {
+    void DrawProp(ref float y, Rect position, SerializedProperty property, string name)
+    {
         SerializedProperty prop = property.FindPropertyRelative(name);
 
         EditorGUI.PropertyField(
@@ -96,7 +112,8 @@ public class AreaAffectsDrawer : PropertyDrawer {
         y += EditorGUIUtility.singleLineHeight + 2;
     }
 
-    public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
         if (!property.isExpanded)
             return EditorGUIUtility.singleLineHeight;
 
@@ -114,7 +131,8 @@ public class AreaAffectsDrawer : PropertyDrawer {
     }
 }
 
-public enum CheckShape {
+public enum CheckShape
+{
     Cube,
     Sphere
 }
