6029beb77d48b89c2c98e5544e3bea5a29eed388
AHannis <s4108628@glos.ac.uk>
2026-04-20 22:16:39 +0100

    starting to fix things

--- Assets/Scenes/Levels/Level_1/CheckPointLevel/ContinueGame.cs
+++ Assets/Scenes/Levels/Level_1/CheckPointLevel/ContinueGame.cs
@@ -1,32 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Collections;
 
 public class ContinueGame : MonoBehaviour {
-    public string lobbySceneName = "Lobby";
-    public int spawnID = 0;
 
-    public bool autoContinue = true;
-    public float delay = 2f;
+    public string nextSceneName = "Lobby";
 
-    void Start() {
-        if (autoContinue) {
-            StartCoroutine(AutoLoad());
-        }
-    }
-
-    IEnumerator AutoLoad() {
-        yield return new WaitForSeconds(delay);
-        LoadLobby();
-    }
-
-    public void LoadLobby() {
-        
-        PlayerPrefs.SetInt("SpawnID", spawnID);
-        PlayerPrefs.Save();
-
-        Debug.Log("Loading Lobby with SpawnID: " + spawnID);
-
-        SceneManager.LoadScene(lobbySceneName);
+    public void Continue() {
+        SceneManager.LoadScene(nextSceneName);
     }
 }
