a310f91e9c4e90c654cdea7bbbee132870542cdd
AHannis <s4108628@glos.ac.uk>
2026-04-20 22:16:39 +0100

    starting to fix things

--- Assets/Scripts/LevelThings/LevelExit.cs
+++ Assets/Scripts/LevelThings/LevelExit.cs
@@ -3,45 +3,26 @@ using UnityEngine.SceneManagement;
 
 public class LevelExit : MonoBehaviour {
 
-    [Header("Scene Settings")]
-    public string nextSceneName;
+    public string nextSceneName = "EndOfLevel";
 
-    [Header("Optional Spawn (leave OFF for UI scenes)")]
-    public bool useSpawnPoint = false;
-    [SerializeField] private Vector3 spawnPositionInNextScene;
+    private bool triggered = false;
 
     private void OnTriggerEnter(Collider other) {
-        Debug.Log("Trigger entered by: " + other.name);
+        if (!other.CompareTag("Player") || triggered) return;
 
-        if (!other.CompareTag("Player")) {
-            Debug.Log("Not player, ignoring.");
-            return;
-        }
+        triggered = true;
 
-        Debug.Log("Player entered level exit.");
-
-
+       
         if (CollectiblesManager.Instance != null) {
             int coins = CollectiblesManager.Instance.GetCoinCount();
             PlayerPrefs.SetInt("FinalCoins", coins);
             Debug.Log("Saved coins: " + coins);
-        } else {
-            Debug.LogWarning("CollectiblesManager not found!");
         }
 
+        PlayerPrefs.SetInt("Stage2Unlocked", 1);
+        PlayerPrefs.Save();
 
-        if (useSpawnPoint) {
-            PlayerPrefs.SetFloat("SpawnX", spawnPositionInNextScene.x);
-            PlayerPrefs.SetFloat("SpawnY", spawnPositionInNextScene.y);
-            PlayerPrefs.SetFloat("SpawnZ", spawnPositionInNextScene.z);
-
-            PlayerPrefs.SetString("SpawnScene", nextSceneName);
-            PlayerPrefs.Save();
-
-            Debug.Log("Spawn saved: " + spawnPositionInNextScene);
-        }
-
-        Debug.Log("Loading scene: " + nextSceneName);
-        SceneManager.LoadScene(1);
+        
+        SceneManager.LoadScene(nextSceneName);
     }
 }
