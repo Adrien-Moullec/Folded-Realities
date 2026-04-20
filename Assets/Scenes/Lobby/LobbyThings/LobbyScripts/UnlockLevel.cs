afd37f21104fa868c5ebc977798fbc6f0629fdaf
AHannis <s4108628@glos.ac.uk>
2026-04-20 19:53:52 +0100

    lobby things and player forced start

---
+++ Assets/Scenes/Lobby/LobbyThings/LobbyScripts/UnlockLevel.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class UnlockLevel : MonoBehaviour {
+    public string unlockKey;
+
+    private bool triggered = false;
+
+    void OnTriggerEnter(Collider other) {
+        if (!other.CompareTag("Player") || triggered) return;
+
+        triggered = true;
+
+        PlayerPrefs.SetInt(unlockKey, 1);
+        PlayerPrefs.Save();
+
+        Debug.Log(unlockKey + " unlocked!");
+    }
+}
