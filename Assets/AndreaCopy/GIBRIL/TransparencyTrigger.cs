2d2bc406ee65a3eee47485124bcb5d94cf7bb62a
AHannis <s4108628@glos.ac.uk>
2026-05-22 09:44:25 +0100

    enable / disable object

--- Assets/AndreaCopy/GIBRIL/TransparencyTrigger.cs
+++ Assets/AndreaCopy/GIBRIL/TransparencyTrigger.cs
@@ -1,18 +1,57 @@
 using UnityEngine;
 
 public class TransparencyTrigger : MonoBehaviour {
+
+    [Header("Transparency")]
     [SerializeField] private ObjectTransparency targetObject;
+
+    [Header("Enable / Disable")]
+    [SerializeField] private GameObject targetGameObject;
+
+    [SerializeField] private bool disableWhenPlayerInside = true;
+
+    [Header("Settings")]
     [SerializeField] private string playerTag = "Player";
 
     private void OnTriggerEnter(Collider other) {
-        if (other.CompareTag(playerTag)) {
+
+        if (!other.CompareTag(playerTag))
+            return;
+
+        // transparency
+        if (targetObject != null) {
             targetObject.SetTransparent(true);
+        }
+
+        // enable / disable object
+        if (targetGameObject != null) {
+
+            if (disableWhenPlayerInside) {
+                targetGameObject.SetActive(false);
+            } else {
+                targetGameObject.SetActive(true);
+            }
         }
     }
 
     private void OnTriggerExit(Collider other) {
-        if (other.CompareTag(playerTag)) {
+
+        if (!other.CompareTag(playerTag))
+            return;
+
+        // transparency
+        if (targetObject != null) {
             targetObject.SetTransparent(false);
+        }
+
+        // enable / disable object
+        if (targetGameObject != null) {
+
+            if (disableWhenPlayerInside) {
+                targetGameObject.SetActive(true);
+            } else {
+                targetGameObject.SetActive(false);
+            }
         }
     }
 }
