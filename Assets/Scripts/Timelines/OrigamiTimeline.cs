fc8ac9249bad00d9207f83bb924edb36a0f7fd22
AdrienMoullec <amoullec1@gmail.com>
2026-05-24 19:47:55 +0100

    Gibril's Timelines

--- Assets/Scripts/Timelines/OrigamiTimeline.cs
+++ Assets/Scripts/Timelines/OrigamiTimeline.cs
@@ -7,30 +7,47 @@ using UnityEngine.Playables;
 
 [RequireComponent(typeof(PlayableDirector))]
 public class OrigamiTimeline : MonoBehaviour {
-    private PlayableDirector playableDirector;
+    [SerializeField] public bool playOnAwake = false;
     [SerializeField] public UnityEvent onStart;
     [SerializeField] public UnityEvent timelineStartEvents;
     [SerializeField] public UnityEvent timelineEndEvents;
-    private void Start() {
+
+    private PlayableDirector playableDirector;
+
+    private void Awake() {
         playableDirector = GetComponent<PlayableDirector>();
         onStart.Invoke();
         playableDirector.played += input => {
             timelineStartEvents.Invoke();
             EntityManager.instance.DeactivateAllEntities();
+            if (TryGetComponent(out Collider c)) c.enabled = false;
         };
         playableDirector.stopped += input => {
             timelineEndEvents.Invoke();
             EntityManager.instance.ActivateAllEntities();
-            GetComponent<Collider>().enabled = false;
-            Debug.Log("END");
+        };
+        if (playOnAwake) playableDirector.Play();
+    }
+
+    void OnDisable() {
+        playableDirector.played -= input => {
+            timelineStartEvents.Invoke();
+            EntityManager.instance.DeactivateAllEntities();
+            if (TryGetComponent(out Collider c)) c.enabled = false;
+        };
+        playableDirector.stopped -= input => {
+            timelineEndEvents.Invoke();
+            EntityManager.instance.ActivateAllEntities();
         };
     }
-    void OnDisable() {
-        playableDirector.played -= input => timelineStartEvents.Invoke();
-        playableDirector.stopped -= input => timelineEndEvents.Invoke();
-    }
+
     public void OnTriggerEnter(Collider other) {
         if (other.tag != "Player") return;
         playableDirector.Play();
+    }
+
+    void OnValidate() {
+        if (playableDirector == null) playableDirector = GetComponent<PlayableDirector>();
+        playableDirector.playOnAwake = false;
     }
 }
