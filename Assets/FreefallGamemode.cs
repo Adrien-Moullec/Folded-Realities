7566cab339809c609b193c15ffcdf1d7632e8066
AdrienMoullec <amoullec1@gmail.com>
2026-05-26 23:10:28 +0100

    Freefall

--- Assets/FreefallGamemode.cs
+++ Assets/FreefallGamemode.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 
 using UnityEngine;
 using UnityEngine.Playables;
@@ -9,21 +10,22 @@ using UnityEngine.Pool;
 public class FreefallGamemode : MonoBehaviour {
     [SerializeField] Transform damageObjectParents;
     [SerializeField] FallGameplayTriggerDamage damageObjects;
-    [SerializeField] Barrage[] barrages;
+    [SerializeField] List<Barrage> barrages;
     [SerializeField] Vector3 point1;
     [SerializeField] Vector3 point2;
     [SerializeField] float DespawnHeight = 10;
     [SerializeField, Min(0.1f)] float Speed = 1;
 
-    ObjectPool<FallGameplayTriggerDamage> pooledObjects;
+    public ObjectPool<FallGameplayTriggerDamage> pooledObjects;
+    int spawnCount = 0;
 
     void Awake() {
         pooledObjects = new ObjectPool<FallGameplayTriggerDamage>(
             createFunc: () => Instantiate(damageObjects, damageObjectParents),
             actionOnGet: damageObj => OnSpawn(damageObj),
-            actionOnRelease: damageObj => damageObj.gameObject.SetActive(false),
+            actionOnRelease: damageObj => Despawn(damageObj),
             actionOnDestroy: damageObj => Destroy(damageObj.gameObject),
-            collectionCheck: true,   // An Editor-only check that determines if an instance is returned back to the pool. Throws an exception if the instance is already in the pool.
+            collectionCheck: true,
             defaultCapacity: 10,
             maxSize: 100);
         StartCoroutine(Gameplay());
@@ -40,20 +42,19 @@ public class FreefallGamemode : MonoBehaviour {
         GetComponent<LevelExit>().NextScene();
     }
     public IEnumerator SpawnBarrage(Barrage barrage) {
-        float t; Vector3 spawnPos;
         for (int i = 0; i < UnityEngine.Random.Range(barrage.countMin, barrage.countMax); i++) {
-            t = UnityEngine.Random.Range(0f, 1f);
-            spawnPos = Vector3.Lerp(point1, point2, t);
-            pooledObjects.Get().OnSpawn(this, spawnPos, UnityEngine.Random.Range(barrage.speedMin, barrage.speedMax));
+            pooledObjects.Get().OnSpawn(this, UnityEngine.Random.Range(barrage.speedMin, barrage.speedMax));
             yield return new WaitForSeconds(UnityEngine.Random.Range(barrage.minInterval, barrage.maxInterval));
         }
     }
 
     public void OnSpawn(FallGameplayTriggerDamage fallGameplayTriggerDamage) {
         fallGameplayTriggerDamage.gameObject.SetActive(true);
+        fallGameplayTriggerDamage.transform.position = Vector3.Lerp(point1, point2, UnityEngine.Random.Range(0f, 1f));
     }
     public void Despawn(FallGameplayTriggerDamage fallGameplayTriggerDamage) {
-        pooledObjects.Release(fallGameplayTriggerDamage);
+        fallGameplayTriggerDamage.gameObject.SetActive(false);
+        fallGameplayTriggerDamage.transform.position = point1;
     }
 
     [Serializable]
