bb0b82ac39a03f6c30515e5a2c850ddb7f4e5931
AdrienMoullec <amoullec1@gmail.com>
2026-05-28 23:37:30 +0100

    Script update

--- Assets/Scripts/AbilitySystem/Ability Controllers/PlayerAbilityController.cs
+++ Assets/Scripts/AbilitySystem/Ability Controllers/PlayerAbilityController.cs
@@ -130,10 +130,19 @@ namespace AbilitySystem {
 
             StartCoroutine(Transition(playerSetSummary));
         }
+
+        /// <summary>
+        /// Transition to next avatar
+        /// </summary>
+        /// <param name="newSummary"> player set of new model </param>
+        /// <returns></returns>
         private IEnumerator Transition(PlayerSetSummary newSummary) {
+
+            /// Only transition if unlocked
             if (!newSummary.isUnlocked) yield break;
             paperParticleDelta?.StartDelta();
 
+            /// Call transform animation
             yield return currentAbilitySet.entityBody.animatorManager.InitiateOneOffAnimation(
                 null,
                 (f) => { paperParticleDelta?.UpdateDelta(f); },
@@ -145,8 +154,10 @@ namespace AbilitySystem {
             );
             SetNewSummary(newSummary);
 
+            /// Start movement logic
             currentAbilitySet.playerAbilitySet.movement.StartUp(currentAbilitySet.entityBody);
 
+            /// Transform in model animation
             yield return currentAbilitySet.entityBody.animatorManager.InitiateOneOffAnimation(
                 null,
                 (f) => { paperParticleDelta?.UpdateDelta(1 - f); },
@@ -156,11 +167,22 @@ namespace AbilitySystem {
                 true
             );
         }
+
+        /// <summary>
+        /// Set new player ability set
+        /// </summary>
+        /// <param name="playerSetSummary"> set to switch to </param>
         private void SetNewSummary(PlayerSetSummary playerSetSummary) {
             currentAbilitySet?.entityBody.animatorManager.gameObject.SetActive(false);
             currentAbilitySet = playerSetSummary;
             currentAbilitySet?.entityBody.animatorManager.gameObject.SetActive(true);
         }
+
+        /// <summary>
+        /// Unlock set by name so it is available to transform into
+        /// </summary>
+        /// <param name="name"> Ability set name </param>
+        /// <returns> true on success </returns>
         public bool UnlockSet(string name) {
             if (TryGetSetSummary(name, out PlayerSetSummary playerSetSummary)) {
                 playerSetSummary.isUnlocked = true;
@@ -168,6 +190,13 @@ namespace AbilitySystem {
             }
             return false;
         }
+
+        /// <summary>
+        /// Try get player set summary by name
+        /// </summary>
+        /// <param name="nameCheck"> Name to check for </param>
+        /// <param name="playerSetSummary"> receive new player set summary if available </param>
+        /// <returns> true on ability set found </returns>
         private bool TryGetSetSummary(string nameCheck, out PlayerSetSummary playerSetSummary) {
             if (playerSetsList.Any(x => x.abilitySetSO.abilitySetName == nameCheck)) {
                 playerSetSummary = playerSetsList.First(x => x.abilitySetSO.abilitySetName == nameCheck);
@@ -179,6 +208,12 @@ namespace AbilitySystem {
         #endregion
 
         #region Movement Functions
+
+        /// <summary>
+        /// Move entity in direction
+        /// </summary>
+        /// <param name="direction"> directional movement </param>
+        /// <param name="rotate"> optional rotate towards direction </param>
         public override void OnMoveEntity(Vector3 direction, bool rotate = true) {
             characterController.Move(direction);
             direction.y = 0;
@@ -190,20 +225,35 @@ namespace AbilitySystem {
             yield return base.OnHitFrames();
             invincible = false;
         }
+
+        /// <summary>
+        /// Rotate entity body towards a direction
+        /// </summary>
         public override void OnRotateEntity(Vector3 direction) {
             if (direction != Vector3.zero) currentAbilitySet.entityBody.bodyHolder.transform.forward = direction;
             transform.rotation = Quaternion.Euler(0, transform.rotation.y, 0);
         }
+
+        /// <summary>
+        /// Set a location for the entity to move towards
+        /// </summary>
         public override void OnEntityTrack(Vector3 location) {
             Debug.Log("TRACK");
         }
         #endregion
 
         #region Data
+        /// <summary>
+        /// Get entity body information
+        /// </summary>
         public override EntityBody GetEntityBody() {
             if (currentAbilitySet != null) return currentAbilitySet.entityBody;
             else return playerSetsList[0].entityBody;
         }
+
+        /// <summary>
+        /// Draw ability gizmos
+        /// </summary>
         public override void OnDrawGizmos() {
             foreach (var n in playerSetsList) {
                 n?.abilitySetSO?.movement?.GizmoEvent(n.entityBody);
@@ -216,14 +266,20 @@ namespace AbilitySystem {
         public override void Die() {
             StartCoroutine(OnDie());
         }
+
+        /// <summary>
+        /// On player die, call animation and start respawn transition
+        /// </summary>
         IEnumerator OnDie() {
 
+            /// Transform back to kuhaku for death
             if (TryGetSetSummary("Kuhaku", out PlayerSetSummary playerSetSummary) && currentAbilitySet?.abilitySetSO.abilitySetName != "Kuhaku")
                 yield return Transition(playerSetSummary);
-
             currentAbilitySet.abilitySetSO.healthSettings?.Die(currentAbilitySet.entityBody, ref CurrentHealth);
 
             GetComponent<CharacterController>().enabled = false;
+
+            /// Transition after Kuhaku death
             bool isFin = false;
             yield return GetEntityBody().animatorManager.InitiateOneOffAnimation(
                 null,
@@ -239,6 +295,8 @@ namespace AbilitySystem {
                 0.2f
             );
             while (!isFin) yield return null;
+
+            /// Respawn Kuhaku to last save location and reset health
             StartCoroutine(GameplaySystem.instance.Respawn());
             GetComponent<CharacterController>().enabled = true;
             currentAbilitySet?
@@ -254,43 +312,67 @@ namespace AbilitySystem {
             foreach (var n in GetEntityBody().entityShader)
                 n.material.SetFloat("_DissolveValue", 0);
         }
+
+        /// <summary>
+        /// Set max health
+        /// </summary>
         public override void SetMaxHealth() {
             base.SetMaxHealth();
             playerHealthCanvas?.UpdateHearts(CurrentHealth);
         }
+
+        /// <summary>
+        /// Heal player by an amount
+        /// </summary>
         public override void Heal(EntityDamage heal) {
             base.Heal(heal);
             foreach (var n in GetEntityBody().entityShader)
                 n.material.SetFloat("_Health01", CurrentHealth / MaxHealth);
             playerHealthCanvas?.UpdateHearts(CurrentHealth);
         }
+
+        /// <summary>
+        /// Damage entity by an amount
+        /// </summary>
         public override void Damage(EntityDamage damage) {
+            /// check to see if damage is from an enemy
             if (EntityTeamFunctions.HasCommonTeam(damage.damagingTeam, entityTeam)) return;
             if (invincible)
                 return;
 
+            /// Do damage that affects shader
             base.Damage(damage);
             playerHealthCanvas?.UpdateHearts(CurrentHealth);
-
             foreach (var n in GetEntityBody().entityShader)
                 n.material.SetFloat("_Health01", CurrentHealth / MaxHealth);
-
             StartCoroutine(OnHitFrames());
+
+            /// Check for death
             if (CurrentHealth <= 0)
                 Die();
         }
 
+        /// <summary>
+        /// Set summary that contains ability and entity information
+        /// </summary>
         [Serializable]
         public class PlayerSetSummary {
+            [Tooltip("Is set unlocked or not.")]
             public bool isUnlocked = false;
+            [Tooltip("Ability set information for a model.")]
             public PlayerAbilitySetSO abilitySetSO;
+            [Tooltip("Entity body information.")]
             public EntityBody entityBody;
+            [Tooltip("Player ability set.")]
             [HideInInspector] public PlayerAbilitySet playerAbilitySet;
         }
         #endregion
     }
 }
 #if UNITY_EDITOR
+/// <summary>
+/// 
+/// </summary>
 [CustomEditor(typeof(PlayerAbilityController))]
 [CanEditMultipleObjects]
 public class PlayerAbilityControllerEditor : Editor {
