d300c29aec575256ada7b473fcd127a1c109a121
AdrienMoullec <amoullec1@gmail.com>
2026-05-28 21:24:27 +0100

    Enemy update

--- Assets/Scripts/AbilitySystem/Ability Sets/EnemyAbilitySetSO.cs
+++ Assets/Scripts/AbilitySystem/Ability Sets/EnemyAbilitySetSO.cs
@@ -7,15 +7,19 @@ namespace AbilitySystem {
     [CreateAssetMenu(fileName = "Ability Set", menuName = MenuAssetNames.AbilitySet + "/Base Enemy Ability Set", order = 0)]
     public class EnemyAbilitySetSO : AbilitySetSO {
         [SerializeField] public CooldownAbilitySO attack;
+        [SerializeField] public CooldownAbilitySO attack2;
     }
 
     [Serializable]
     public class EnemyAbilitySet : AbilitySet {
         [SerializeField] public CooldownAbilitySummary attack;
+        [SerializeField] public CooldownAbilitySummary attack2;
 
         public EnemyAbilitySet(EnemyAbilitySetSO abilitySet, EntityBody eb) : base(abilitySet.name, abilitySet.movement, abilitySet.healthSettings, eb) {
             if (abilitySet.attack != null)
                 attack = new(abilitySet.attack, eb);
+            if (abilitySet.attack2 != null)
+                attack2 = new(abilitySet.attack2, eb);
         }
     }
 }
